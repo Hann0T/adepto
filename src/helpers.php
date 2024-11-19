7e15ed95c597c899f5e1a5ac920db6e32dcc5bc9
hans <hansolivas1@gmail.com>
2024-11-19 18:20:44 -0500

    feat: dd helper.

---
+++ src/helpers.php
@@ -0,0 +1,10 @@
+<?php
+if (!function_exists('dd')) {
+    function dd(...$args)
+    {
+        echo '<pre style="background: #f4f4f4; border: 1px solid #ddd; border-left: 3px solid #f36d33; color: #666; page-break-inside: avoid; font-family: monospace; font-size: 15px; line-height: 1.6; margin-bottom: 1.6em; max-width: 100%; overflow: auto; padding: 1em 1.5em; display: block; word-wrap: break-word;">';
+        call_user_func_array('var_dump', $args);
+        echo '</pre>';
+        die();
+    }
+}
