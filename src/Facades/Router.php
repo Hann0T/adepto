3ad16e2fe9f9375e8a982268bd6bdc6c615a3234
hans <hansolivas1@gmail.com>
2024-11-24 01:22:35 -0500

    refactor: Router facade PHPDoc.

--- src/Facades/Router.php
+++ src/Facades/Router.php
@@ -2,6 +2,11 @@
 
 namespace Adepto\Facades;
 
+/**
+ * @method static void get(string $route, callable $callback)
+ * @method static void post(string $route, callable $callback)
+ * @method static callable resolve($request)
+ */
 class Router extends Facade
 {
     public static function getFacadeAccessor()
