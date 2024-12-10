6ad79493fa4b7769bcb59fabc332d17e5cd8dd29
hans <hansolivas1@gmail.com>
2024-12-09 23:21:36 -0500

    feat: dd with debug trace.

--- src/helpers.php
+++ src/helpers.php
@@ -8,6 +8,7 @@ if (!function_exists('dd')) {
     function dd(...$args)
     {
         echo '<pre style="background: #f4f4f4; border: 1px solid #ddd; border-left: 3px solid #f36d33; color: #666; page-break-inside: avoid; font-family: monospace; font-size: 15px; line-height: 1.6; margin-bottom: 1.6em; max-width: 100%; overflow: auto; padding: 1em 1.5em; display: block; word-wrap: break-word;">';
+        echo '<span style="display: block;">' . debug_backtrace()[0]['file'] . ':' . debug_backtrace()[0]['line'] . '</span>';
         call_user_func_array('var_dump', $args);
         echo '</pre>';
         die();
